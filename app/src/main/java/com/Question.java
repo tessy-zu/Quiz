531c8b2990dfd0507c8d266d234e2e9421200482
tessy-zu <tessy.zubenko@gmail.com>
2017-11-30 20:44:35 +0200

    added 3 buttons with text and click listeners

--- app/src/main/java/com/Question.java
+++ app/src/main/java/com/Question.java
@@ -5,4 +5,28 @@ package com;
  */
 
 public class Question {
+
+    private int textId;
+    private boolean answer;
+
+    public Question(int textId, boolean answer) {
+        this.textId = textId;
+        this.answer = answer;
+    }
+
+    public int getTextId() {
+        return this.textId;
+    }
+
+    public void setTextId(int textId) {
+        this.textId = textId;
+    }
+
+    public boolean answer() {
+        return this.answer;
+    }
+
+    public void setAnswer(boolean answer) {
+        this.answer = answer;
+    }
 }
--- app/src/main/java/com/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/android/geoquiz/QuizActivity.java
@@ -4,12 +4,27 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.Question;
 
 public class QuizActivity extends AppCompatActivity {
 
-    private Button mTrueButton;
-    private Button mFalseButton;
+    private Button TrueButton;
+    private Button FalseButton;
+    private Button NextButton;
+    private TextView QuestionTextView;
+
+    private Question[] Questions = new Question[]{
+            new Question(R.string.question_oceans, true),
+            new Question(R.string.question_mideast, false),
+            new Question(R.string.question_africa, false),
+            new Question(R.string.question_americas, true),
+            new Question(R.string.question_asia, true)
+    };
+
+    private int index = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,19 +32,37 @@ public class QuizActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
-        mTrueButton = (Button) findViewById(R.id.true_button);
-        mTrueButton.setOnClickListener(new View.OnClickListener() {
+        QuestionTextView = (TextView) findViewById(R.id.question_text_view);
+        int question = Questions[index].getTextId();
+        QuestionTextView.setText(question);
+
+        TrueButton = (Button) findViewById(R.id.true_button);
+        FalseButton = (Button) findViewById(R.id.false_button);
+        TrueButton.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+                Toast.makeText(QuizActivity.this,
+                        Questions[index].answer() ? R.string.toast_correct : R.string.toast_incorrect,
+                        Toast.LENGTH_SHORT).show();
             }
         });
 
-        mFalseButton = (Button) findViewById(R.id.false_button);
-        mFalseButton.setOnClickListener(new View.OnClickListener() {
+        FalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+                Toast.makeText(QuizActivity.this, R.string.toast_correct, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        NextButton = (Button) findViewById(R.id.next_button);
+        NextButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                index = (index + 1) % Questions.length;
+                int question = Questions[index].getTextId();
+                QuestionTextView.setText(question);
             }
         });
     }
