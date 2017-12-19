c9bbe7da01d9d76c677dc5186f9d20f58579f550
tessy-zu <tessy.zubenko@gmail.com>
2017-12-19 19:25:36 +0200

    Landscape orientation added

--- app/src/main/java/com/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/android/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.android.geoquiz;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -11,12 +12,16 @@ import com.Question;
 
 public class QuizActivity extends AppCompatActivity {
 
+    private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
+
     private Button trueButton;
     private Button falseButton;
     private Button nextButton;
+    private Button previousButton;
     private TextView questionTextView;
 
-    private Question[] questions = new Question[] {
+    private Question[] questions = new Question[]{
             new Question(R.string.question_oceans, true),
             new Question(R.string.question_mideast, false),
             new Question(R.string.question_africa, false),
@@ -26,6 +31,13 @@ public class QuizActivity extends AppCompatActivity {
 
     private int index = 0;
 
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        savedInstanceState.putInt(KEY_INDEX, index);
+    }
+
     private void updateQuestion() {
         questionTextView = (TextView) findViewById(R.id.question_text_view);
         int question = questions[index].getTextId();
@@ -34,14 +46,14 @@ public class QuizActivity extends AppCompatActivity {
 
     private void checkAnswer(boolean userPressedTrue) {
         boolean answerIsTrue = questions[index].answer();
-        int messageId = 0;
+        int messageId;
 
         if (userPressedTrue == answerIsTrue) {
             messageId = R.string.toast_correct;
-            } else {
+        } else {
             messageId = R.string.toast_incorrect;
-            }
-            Toast.makeText(this, messageId, Toast.LENGTH_SHORT).show();
+        }
+        Toast.makeText(this, messageId, Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -49,10 +61,13 @@ public class QuizActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+        Log.d(TAG, "onCreate(Bundle) called");
+        setContentView(R.layout.activity_quiz);
 
         trueButton = (Button) findViewById(R.id.true_button);
         falseButton = (Button) findViewById(R.id.false_button);
         nextButton = (Button) findViewById(R.id.next_button);
+        previousButton = (Button) findViewById(R.id.previous_button);
 
         trueButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -76,5 +91,48 @@ public class QuizActivity extends AppCompatActivity {
             }
         });
         updateQuestion();
+
+        previousButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                index = (index - 1) % questions.length;
+                updateQuestion();
+            }
+        });
+        if (savedInstanceState != null) {
+            index = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
+        updateQuestion();
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+    }
+
 }
